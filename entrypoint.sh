b9d4518f481c94e7415508ca1146507598a7cbc7
lrdsc <louis.rousselot-de-saint-ceran@artefact.com>
2022-03-24 10:45:52 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r $3
-pyflyte -c $1 package --in-container-source-path /root --image $2
+pyflyte -c $1 --pkgs $4 package --in-container-source-path /root --image $2 -s $5 -o $5/flyte-package.tgz
